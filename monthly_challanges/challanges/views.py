59ae4b5cffb2555779081361478a1f476a72b417
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-05-07 17:24:20 +0530

    this is non dynamical allting of urls for views

--- monthly_challanges/challanges/views.py
+++ monthly_challanges/challanges/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def jan(request):
+    return HttpResponse("jan")
+def feb(request):
+    return HttpResponse("feb")
+
