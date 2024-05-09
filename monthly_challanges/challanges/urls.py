3d8d06f5df316aa5d1408bd35b2f84dc2ab3677d
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-05-09 15:02:47 +0530

    app completed without css

--- monthly_challanges/challanges/urls.py
+++ monthly_challanges/challanges/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from.import views
 urlpatterns = [ 
+  path("",views.index),#/challanges/nothing will be triggered  
   path("<int:month>",views.monthly_ch_by_num) ,
   path("<str:month>",views.monthly_ch,name="month-challenge")
 ]
--- monthly_challanges/challanges/views.py
+++ monthly_challanges/challanges/views.py
@@ -1,26 +1,38 @@
-from django.http import HttpResponse, HttpResponseNotFound,HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
 monthly_cha = {
-    "janaury": "January",
+    "january": "January",
     "february": "February",
     "march": "March",
     "april": "April"
-
 }
 
-# Create your views here.
-def monthly_ch_by_num(request,month):
-    months=list(monthly_cha.keys())
+def index(request):
+    list_items = ""
+    months = list(monthly_cha.keys())
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
 
-    redirect_month=months[month-1]
-    redirect_path=reverse("month-challenge",args=[redirect_month])
-    return HttpResponseRedirect(redirect_path)
+# Create your views here.
+def monthly_ch_by_num(request, month):
+    months = list(monthly_cha.keys())
+    if 1 <= month <= len(months):
+        redirect_month = months[month - 1]
+        redirect_path = reverse("month-challenge", args=[redirect_month])
+        return HttpResponseRedirect(redirect_path)
+    else:
+        return HttpResponseNotFound("Invalid month number")
 
 def monthly_ch(request, month):
     try:
         challenge_text = monthly_cha[month]
-        return HttpResponse(challenge_text)
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     except KeyError:
         return HttpResponseNotFound("Not valid")
