275fc26ac33947d2c16d67e888fb5b031dc42adc
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-05-15 12:13:30 +0530

    the slug is nt wroking properly

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.starting_page, name='starting-page'),  # Use empty string for root path
     path('posts/', views.posts, name='posts'),
-    path('posts/<slug:slug>/', views.post_detail, name='post-detail-page'),
+    path('posts/<slug:slug>/', views.post_detail, name='post-detail-page')
 ]
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 def starting_page(request):
-    return render(request,"blog/index.html")
-def posts(request):
-    pass
-def post_detail(request):
-    pass
+    return render(request, "blog/index.html")
 
+def posts(request):
+    return render(request, "blog/all-posts.html")
+
+def post_detail(request, slug):
+    return render(request, "blog/post-detail.html")
