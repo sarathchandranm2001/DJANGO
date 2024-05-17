5ef0831921cb483216b6129594c1fc02b583c8dd
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-05-17 22:44:53 +0530

    navigation link to books

--- book_store/book_outlet/urls.py
+++ book_store/book_outlet/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
-    path("<int:id>", views.book_details, name='book_details'),
+    path("<int:id>", views.book_details, name='book-details'),
 ]
