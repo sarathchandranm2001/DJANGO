e5b20cb6a5ecd757889b72359b336fe9c308013a
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-05-17 22:52:20 +0530

    another way of navigation using models

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator,MaxValueValidator
+from django.urls import reverse
 
 # Create your models here.
 
@@ -9,6 +10,10 @@ class Book(models.Model):
         validators=[MinValueValidator(1),MaxValueValidator(5)])
     author =models.CharField(null=True,max_length=100)
     is_bestselling=models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        return reverse("book-details", args=[self.id])#other way of giving url as links in stead of href so im commenting it there on views.py
+    
 
 def __str__(self):
     return f"{self.title}({self.rating})"
