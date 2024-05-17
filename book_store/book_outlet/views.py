ffd9f1e600cb34b5d5015a9cd7f760f147bb80f0
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-05-17 22:40:35 +0530

    structure of book_outlet and datas from db are shown in the template

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import Http404, HttpResponse
+from .models import Book
 
-# Create your views here.
+
+def index(request):
+    books=Book.objects.all()
+    return render(request, 'book_outlet/index.html',{
+        'books':books
+    })
+
+def book_details(request, id):
+    try:
+        book = Book.objects.get(id=id)
+    except:
+        raise Http404    
+    return render(request, 'book_outlet/book_details.html', {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestseller": book.is_bestselling
+    })
