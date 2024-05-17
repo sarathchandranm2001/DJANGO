a64e853e38df2f4594c70c468bb3705c2c2e1e5d
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-05-18 00:29:34 +0530

    aggregations like count,max,min,avg

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render
-from django.http import Http404, HttpResponse
+from django.http import Http404
+from django.db.models import Avg, Max, Min
 from .models import Book
 
-
 def index(request):
-    books=Book.objects.all()
-    return render(request, 'book_outlet/index.html',{
-        'books':books
+    books = Book.objects.all()
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg('rating'))['rating__avg']
+    return render(request, 'book_outlet/index.html', {
+        'books': books,
+        'total_books': num_books,
+        'avg_rating': avg_rating
     })
 
 def book_details(request, id):
     try:
         book = Book.objects.get(id=id)
     except:
-        raise Http404    
+        raise Http404
     return render(request, 'book_outlet/book_details.html', {
         "title": book.title,
         "author": book.author,
