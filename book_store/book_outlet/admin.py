15cf99bf00c3cbfe94fcf7134131230530e3ddba
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-05-18 13:22:26 +0530

    Relationship in Admin-also corrected names for objects

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Book
+from .models import Author, Book
 # Register your models here.
 
 class BookAdmin(admin.ModelAdmin):
     list_filter=("author","rating")
 admin.site.register(Book,BookAdmin)
+admin.site.register(Author)
--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -8,19 +8,23 @@ class Author(models.Model):
     first_name=models.CharField(max_length=100)
     last_name=models.CharField(max_length=100)
 
+    def full_name(self):
+        return self.first_name + "" + self.last_name
+
+    def __str__(self):
+       return self.full_name()
+
 
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating=models.IntegerField(
         validators=[MinValueValidator(1),MaxValueValidator(5)])
-    author =models.ForeignKey(Author,on_delete=models.CASCADE,null=True)
+    author =models.ForeignKey(Author,on_delete=models.CASCADE,null=True,related_name="books")
     is_bestselling=models.BooleanField(default=False)
 
     def get_absolute_url(self):
         return reverse("book-details", args=[self.id])#other way of giving url as links in stead of href so im commenting it there on views.py
-    
-
-def __str__(self):
-    return f"{self.title}({self.rating})"
+    def __str__(self):
+        return f"{self.title}({self.rating})"#to rename object 1 to original name
 
