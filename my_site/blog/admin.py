25203e3e047118f806dc10f395a94fc4d1dd64c4
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-05-18 22:16:30 +0530

    prepopulated slug and addedpostadmin for filters

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,9 +1,15 @@
+from lib2to3.fixes import fix_filter
 from django.contrib import admin
 
 from .models import Post,Author,Tag
 # Register your models here.
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_filter=("author","tags","date",)
+    list_display = ('title','date','author',)
+    prepopulated_fields={"slug":("title",)}
+
+admin.site.register(Post,PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
 
