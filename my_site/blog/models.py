81c13b93f594ed098b078512ac976405a03971ca
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-05-18 21:54:41 +0530

    added Tag model and Also created admin

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
 
+class Tag(models.Model):
+    captions=models.CharField(max_length=20)
+
+
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_adress=models.EmailField()
+    #maynot be working
 class Post(models.Model):
     title=models.CharField(max_length=150)
     excerpt=models.CharField(max_length=200)
@@ -9,3 +18,5 @@ class Post(models.Model):
     date=models.DateField(auto_now=True)
     slug=models.SlugField(unique=True,db_index=True)
     content=models.TextField(validators=[MinLengthValidator(10)])
+    author=models.ForeignKey(Author,on_delete=models.SET_NULL,related_name="posts",null=True)
+    tags=models.ManyToManyField(Tag)
