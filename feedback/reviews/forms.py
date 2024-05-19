57311761e0c25470259f9ff32ac2359177e438ab
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-05-19 17:41:48 +0530

    manually adding form details

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -3,5 +3,11 @@ from django import forms
 class ReviewForm(forms.Form):
     name=forms.CharField(label="Your Name",max_length=10,error_messages={
         "required:":"Your name must not be empty",
-            "max_length":"please enter a shorter name"    })#only works when we removw required fromm default form using inspect in browser
+            "max_length":"please enter a shorter name"    })
+    #only works when we removw required fromm default form using inspect in browser
+
+    review_text=forms.CharField(label="Your Feed Back :",widget=forms.Textarea,max_length=200)
+    rating=forms.IntegerField(label="Your Rating",min_value=1,max_value=5)
+    
+
     
