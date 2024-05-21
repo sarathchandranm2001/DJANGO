efd5a7873341b9e7361c5ccf88ef97dbee421357
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-05-22 00:21:27 +0530

    ListView

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.ReviewView.as_view(), name='review'),  # class-based view implementation
     path('thank-you', views.ThankYouView.as_view()),
+    path('reviews', views.ReviewsListView.as_view(), name='review-list'),
     path('reviews/<int:id>/', views.SingleReviewView.as_view(), name='single-review'),    
 ]
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,9 +1,15 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import ReviewForm
-from .models import Review
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
+from .forms import ReviewForm
+from .models import Review
+
+
 
 # Create your views here.
 class ReviewView(View):
@@ -36,15 +42,23 @@ class ThankYouView(TemplateView):
         context["message"] = 'This works'
         return context
 
-class ReviewsListView(TemplateView):
+class ReviewsListView(ListView):
     template_name = 'reviews/review_list.html'
+    model=Review
+    context_object_name="reviews"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        reviews = Review.objects.all()
-        context["reviews"] = reviews
-        return context
+    #def get_queryset(self):
+     #   base_query=super().get_queryset()
+      #  data=base_query.filter(rating__gt=4)
+       # return data
+
+   # def get_context_data(self, **kwargs):
+    #    context = super().get_context_data(**kwargs)
+     #   reviews = Review.objects.all()
+      #  context["reviews"] = reviews
+       # return context
     
+
 class SingleReviewView(TemplateView):
     template_name = 'reviews/single_review.html'
     def get_context_data(self, **kwargs):
