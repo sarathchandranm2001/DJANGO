fe7f846be42a4baff9323839bc2a7b31b0c2462c
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-05-22 00:40:10 +0530

    Form-view

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -6,23 +6,32 @@ from .forms import ReviewForm
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView,DetailView
+from django.views.generic.edit import FormView 
 from .forms import ReviewForm
 from .models import Review
 
 
 
 # Create your views here.
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
-        return render(request, 'reviews/review.html', {"form": form})
+class ReviewView(FormView):
+    form_class = ReviewForm
+    template_name="reviews/review.html"
+    success_url="/thank-you"
 
-    def post(self, request):
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank-you")
-        return render(request, 'reviews/review.html', {"form": form})
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+    
+    #def get(self, request):
+     #   form = ReviewForm()
+      #  return render(request, 'reviews/review.html', {"form": form})
+
+    #def post(self, request):
+     #   form = ReviewForm(request.POST)
+      #  if form.is_valid():
+       #     form.save()
+        #    return HttpResponseRedirect("/thank-you")
+        #return render(request, 'reviews/review.html', {"form": form})
 
 def review(request):
     if request.method == 'POST':
