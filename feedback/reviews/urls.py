01130582ff0ef5b959f541b555c9097021e3629d
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-05-21 23:51:23 +0530

    Showing a Detail Template using Class Views

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .import views
+from . import views
 
 urlpatterns = [
-    path('', views.ReviewView.as_view(), name='review'),#class based views implimentation
-    path('thank-you',views.ThankYouView.as_view())
+    path('', views.ReviewView.as_view(), name='review'),  # class-based view implementation
+    path('thank-you', views.ThankYouView.as_view()),
+    path('reviews/<int:id>/', views.SingleReviewView.as_view(), name='single-review'),    
 ]
