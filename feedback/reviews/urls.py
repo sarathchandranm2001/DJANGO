3344eeeca411ed62461c1874f92a0ae71c35d623
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-05-21 23:14:40 +0530

    More Specific Template View-using external class in TemplateViews

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.review, name='review'),
-    path('thank-you',views.thank_you)
+    path('', views.ReviewView.as_view(), name='review'),#class based views implimentation
+    path('thank-you',views.ThankYouView.as_view())
 ]
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -2,18 +2,34 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import ReviewForm
 from .models import Review
+from django.views import View
+from django.views.generic.base import TemplateView
+
 # Create your views here.
+
+class ReviewView(View):
+   def get(self,request):
+      form = ReviewForm()
+      return render(request,'reviews/review.html',{
+        "form":form
+    })
+   def post(self,request):
+       form=ReviewForm(request.POST)
+
+       if form.is_valid():
+          form.save()
+          return HttpResponseRedirect("/thank-you")
+
+          
+
 
 def review(request):
     if request.method=='POST':
-        form=ReviewForm(request.POST)
 
+        form=ReviewForm(request.POST)
         if form.is_valid():
-         review =Review(name=form.cleaned_data['name'],
-                        review_text=form.cleaned_data['review_text'],
-                        rating=form.cleaned_data['rating'])
-         review.save()
-         return HttpResponseRedirect("/thank-you")
+         form.save()
+        return HttpResponseRedirect("/thank-you")
     else :
        form=ReviewForm()
        return render(request,'reviews/review.html',{
@@ -22,8 +38,12 @@ def review(request):
 
 
         
-
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["messages"] ='This works' 
+        return context
+    
  
-def thank_you(request):
-    return render(request, 'reviews/thank_you.html')
 
