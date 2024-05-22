90c1ca08ba95081f5c8eb973a493dae7c9b37bbb
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-05-22 15:45:19 +0530

    FIle input

--- feedback/feedback/urls.py
+++ feedback/feedback/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for feedback project.
+"""feedback URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include("reviews.urls"))
+    path("", include("reviews.urls")),
+    path("profiles/", include("profiles.urls"))
 ]
---
+++ feedback/profiles/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponseRedirect
+
+
+
+# Create your views here.
+
+
+class CreateProfileView(View):
+    def get(self, request):
+        return render(request, "profiles/create_profile.html")
+
+    def post(self, request):
+       print(request.FILES["image"])
+       return HttpResponseRedirect("/profiles")
